e6c4d453c7d508d2ec75101e71b897f12ad72fc8
Abhishek Sen <sen.abhishek@gmail.com>
2014-01-07 19:22:39 +0100

    Initial commit

---
+++ dataset_visualization.m
@@ -0,0 +1,107 @@
+close all;
+
+% Plot input vs output
+figure(1);
+% Dataset 7
+subplot(3,2,1);
+plot(data1(1,:), data1(2,:));
+subplot(3,2,2);
+plot(data1(1,:), data1(3,:));
+subplot(3,2,3);
+plot(data1(1,:), data1(4,:));
+subplot(3,2,4);
+plot(data1(1,:), data1(5,:));
+subplot(3,2,5);
+plot(data1(1,:), data1(6,:));
+subplot(3,2,6);
+plot(data1(1,:), data1(7,:));
+
+figure(2);
+% Dataset 7
+subplot(3,2,1);
+plot(data2(1,:), data2(2,:));
+subplot(3,2,2);
+plot(data2(1,:), data2(3,:));
+subplot(3,2,3);
+plot(data2(1,:), data2(4,:));
+subplot(3,2,4);
+plot(data2(1,:), data2(5,:));
+subplot(3,2,5);
+plot(data2(1,:), data2(6,:));
+subplot(3,2,6);
+plot(data2(1,:), data2(7,:));
+
+figure(3);
+% Dataset 7
+subplot(3,2,1);
+plot(data3(1,:), data3(2,:));
+subplot(3,2,2);
+plot(data3(1,:), data3(3,:));
+subplot(3,2,3);
+plot(data3(1,:), data3(4,:));
+subplot(3,2,4);
+plot(data3(1,:), data3(5,:));
+subplot(3,2,5);
+plot(data3(1,:), data3(6,:));
+subplot(3,2,6);
+plot(data3(1,:), data3(7,:));
+
+figure(4);
+% Dataset 7
+subplot(3,2,1);
+plot(data4(1,:), data4(2,:));
+subplot(3,2,2);
+plot(data4(1,:), data4(3,:));
+subplot(3,2,3);
+plot(data4(1,:), data4(4,:));
+subplot(3,2,4);
+plot(data4(1,:), data4(5,:));
+subplot(3,2,5);
+plot(data4(1,:), data4(6,:));
+subplot(3,2,6);
+plot(data4(1,:), data4(7,:));
+
+figure(5);
+% Dataset 7
+subplot(3,2,1);
+plot(data5(1,:), data5(2,:));
+subplot(3,2,2);
+plot(data5(1,:), data5(3,:));
+subplot(3,2,3);
+plot(data5(1,:), data5(4,:));
+subplot(3,2,4);
+plot(data5(1,:), data5(5,:));
+subplot(3,2,5);
+plot(data5(1,:), data5(6,:));
+subplot(3,2,6);
+plot(data5(1,:), data5(7,:));
+
+figure(6);
+% Dataset 7
+subplot(3,2,1);
+plot(data6(1,:), data6(2,:));
+subplot(3,2,2);
+plot(data6(1,:), data6(3,:));
+subplot(3,2,3);
+plot(data6(1,:), data6(4,:));
+subplot(3,2,4);
+plot(data6(1,:), data6(5,:));
+subplot(3,2,5);
+plot(data6(1,:), data6(6,:));
+subplot(3,2,6);
+plot(data6(1,:), data6(7,:));
+
+figure(7);
+% Dataset 7
+subplot(3,2,1);
+plot(data7(1,:), data7(2,:));
+subplot(3,2,2);
+plot(data7(1,:), data7(3,:));
+subplot(3,2,3);
+plot(data7(1,:), data7(4,:));
+subplot(3,2,4);
+plot(data7(1,:), data7(5,:));
+subplot(3,2,5);
+plot(data7(1,:), data7(6,:));
+subplot(3,2,6);
+plot(data7(1,:), data7(7,:));
